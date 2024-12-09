6518b72033bda26c38e17bd9b76998ac08a4887c
mdsalsabil <salsabilt2@gmail.com>
2024-12-09 03:18:21 +0100

    User Authentifications Full Complete

---
+++ Film_Bluff/accounts/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect # type: ignore
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm # type: ignore
+from django.contrib.auth import login, logout # type: ignore
+from django.contrib import messages # type: ignore
+
+# User Registration
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Registration successful. You can now log in.')
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+# User Login
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            messages.success(request, f'Welcome, {user.username}!')
+            return redirect('home')  # Redirect to homepage after login
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
+
+# User Logout
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'You have been logged out.')
+    return redirect('login')
